67d3e914e3c1cb82556183e766ba1a13198456d9
Katrina Owen <katrina.owen@gmail.com>
2014-03-01 12:07:43 -0700

    Implement homework
    Homework represents a collection of lessons.

---
+++ lib/xapi/homework.rb
@@ -0,0 +1,27 @@
+module Xapi
+  class Homework
+    attr_reader :data, :languages, :progression, :path
+
+    def initialize(data, languages, progression, path=default_path)
+      data.default_proc = Proc.new {|hash, language| hash[language] = []}
+      @data = data
+      @languages = languages
+      @progression = progression
+      @path = path
+    end
+
+    def exercises
+      lessons.map(&:exercises).flatten
+    end
+
+    def lessons
+      languages.map {|language|
+        Lesson.new(data[language], progression.new(language, path))
+      }
+    end
+
+    def default_path
+      '.'
+    end
+  end
+end
