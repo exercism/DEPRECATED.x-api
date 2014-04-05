585dc22f9c72321a4a892672b2c8cb73651233f6
Katrina Owen <katrina.owen@gmail.com>
2014-04-05 00:02:00 -0400

    Move path declaration into progression

--- lib/xapi/course.rb
+++ lib/xapi/course.rb
@@ -2,22 +2,17 @@ module Xapi
   class Course
     attr_reader :data, :languages, :progression, :path
 
-    def initialize(data, languages, progression, path=default_path)
+    def initialize(data, languages, progression)
       data.default_proc = Proc.new {|hash, language| hash[language] = []}
       @data = data
       @languages = languages
       @progression = progression
-      @path = path
     end
 
     def lessons
       languages.map {|language|
-        Lesson.new(data[language], progression.new(language, path))
+        Lesson.new(data[language], progression.new(language))
       }
-    end
-
-    def default_path
-      '.'
     end
   end
 end
