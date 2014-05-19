619010165d069366b5e4a5d9358e6ac476a70251
Katrina Owen <katrina.owen@gmail.com>
2014-05-18 18:49:25 -0600

    Fix content-type for the homepage

--- lib/app/routes/home.rb
+++ lib/app/routes/home.rb
@@ -1,6 +1,10 @@
 module Xapi
   module Routes
-    class Home < Core
+    class Home < Sinatra::Base
+      configure do
+        set :root, './lib/app'
+      end
+
       get '/' do
         haml :markdown, locals: {markdown: Xapi::Markdown.at('./CONTRIBUTING.md')}
       end
