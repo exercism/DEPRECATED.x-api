ee1075eb86c34e81f4e973215a6785467e3bcb68
Katrina Owen <katrina.owen@gmail.com>
2014-06-21 12:00:04 -0700

    Activate swift track

--- test/xapi/config_test.rb
+++ test/xapi/config_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
 require 'json'
 require 'xapi/config'
+require 'xapi/track'
 
 class ConfigTest < Minitest::Test
   # Acceptance test -- actual production languages
@@ -9,7 +10,7 @@ class ConfigTest < Minitest::Test
       "clojure", "coffeescript", "csharp", "elixir",
       "fsharp", "go", "haskell", "javascript",
       "objective-c", "ocaml", "perl5",
-      "python", "ruby", "scala"
+      "python", "ruby", "scala", "swift"
     ].sort
     assert_equal expected, Xapi::Config.languages.sort
   end
