9aa2f1d085e23316298fe8709f5c3ed8baf344b4
Katrina Owen <katrina.owen@gmail.com>
2014-11-01 09:41:10 -0400

    Simplify to use guard in route helper

--- lib/app/helpers/guards.rb
+++ lib/app/helpers/guards.rb
@@ -2,9 +2,9 @@ module Xapi
   module Helpers
     module Guards
       def require_key
-        unless params[:key]
-          halt 401, {error: "Please provide your Exercism.io API key"}.to_json
-        end
+        return if params[:key]
+
+        halt 401, {error: "Please provide your Exercism.io API key"}.to_json
       end
     end
   end
