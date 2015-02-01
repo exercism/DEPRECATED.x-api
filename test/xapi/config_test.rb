9cf823add78733f7cf82554acb3b419a85306645
Katrina Owen <katrina.owen@gmail.com>
2015-02-01 08:29:50 -0800

    Update submodules

--- test/xapi/config_test.rb
+++ test/xapi/config_test.rb
@@ -9,7 +9,7 @@ class ConfigTest < Minitest::Test
     expected = [
       "clojure", "coffeescript", "cpp", "csharp", "elixir",
       "erlang", "fsharp", "go", "haskell", "java", "javascript",
-      "lisp", "lua", "objective-c", "ocaml", "perl5",
+      "lisp", "lua", "objective-c", "ocaml", "perl5", "plsql",
       "python", "ruby", "scala", "swift"
     ].sort
     assert_equal expected, Xapi::Config.languages.sort
