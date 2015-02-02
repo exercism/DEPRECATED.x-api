694b387d950b3365ad8f75906b7437ac2c4aea3b
Katrina Owen <katrina.owen@gmail.com>
2015-02-01 23:16:35 -0800

    Fix incorrect default path for problem readme

--- lib/xapi/readme.rb
+++ lib/xapi/readme.rb
@@ -3,7 +3,7 @@ module Xapi
   class Readme
     attr_reader :slug, :dir, :help
 
-    def initialize(slug, dir="./assignments", help=nil)
+    def initialize(slug, dir="./problems", help=nil)
       @slug = slug
       @dir = dir
       @help = help
