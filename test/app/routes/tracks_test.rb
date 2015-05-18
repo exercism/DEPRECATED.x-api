b8117e9c48e24b6da7c5f5318354cc3effa085d3
Katrina Owen <katrina.owen@gmail.com>
2015-05-17 22:22:14 -0700

    Fix incorrect test name

--- test/app/routes/tracks_test.rb
+++ test/app/routes/tracks_test.rb
@@ -10,7 +10,7 @@ class AppRoutesTracksTest < Minitest::Test
 
   # More of a smoke test than anything else.
   # It will break as we add new languages.
-  def test_some_other_error
+  def test_all_the_tracks
     get '/tracks'
     tracks = JSON.parse(last_response.body)['tracks'].map { |track| track['slug'] }
     Approvals.verify(tracks, name: 'get_tracks')
