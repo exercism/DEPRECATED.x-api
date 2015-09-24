76474a6d925b147db03a920a1e8fdfa0049b75ca
Katrina Owen <katrina.owen@gmail.com>
2015-09-23 18:53:06 -0600

    Add hello-world metadata to fixtures

--- test/xapi/metadata_test.rb
+++ test/xapi/metadata_test.rb
@@ -4,7 +4,7 @@ require 'xapi/metadata'
 class MetadataTest < Minitest::Test
   def test_all
     metadata = Xapi::Metadata.load("./test/fixtures")
-    slugs = %w(apple banana cherry dog one two three).sort
+    slugs = %w(hello-world apple banana cherry dog one two three).sort
     assert_equal slugs, metadata.summaries.map(&:slug).sort
     assert_equal slugs, metadata.slugs.sort
   end
