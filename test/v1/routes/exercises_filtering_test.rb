f498d2fa91d97a12387e23a5f047d3d7a86276c4
Ben Biddington <ben.biddington@gmail.com>
2015-12-08 14:20:42 +1300

    Adjust for rubocop

--- test/v1/routes/exercises_filtering_test.rb
+++ test/v1/routes/exercises_filtering_test.rb
@@ -9,7 +9,7 @@ class V1RoutesExerciseFilteringTest < Minitest::Test
     V1::App
   end
 
-  # 
+  #
   # Right now we have /v2/exercises and /v2/exercises/:track_id. Let's tweak the /v2/exercises to
   # take an optional parameter ?tracks=a,b,c and if that parameter is passed, only return the data for those tracks.
   #
@@ -17,36 +17,36 @@ class V1RoutesExerciseFilteringTest < Minitest::Test
   def test_that_you_may_filter_to_single_track
     getting '/v2/exercises?tracks=fruit'
 
-    returns_tracks ['fruit']
+    returns_tracks %w( fruit )
   end
 
   def test_that_you_may_filter_to_multiple_tracks
     getting '/v2/exercises?tracks=fruit,animal'
 
-    returns_tracks ['animal', 'fruit']
+    returns_tracks %w( animal fruit )
   end
 
   def test_that_empty_filter_returns_all
     getting '/v2/exercises?tracks='
 
-    returns_tracks ['animal', 'fake', 'fruit']
+    returns_tracks %w( animal fake fruit )
   end
 
   def test_that_missing_filter_returns_all
     getting '/v2/exercises'
 
-    returns_tracks ['animal', 'fake', 'fruit']
+    returns_tracks %w( animal fake fruit )
   end
 
   def test_that_blank_space_is_ignored
     getting '/v2/exercises?tracks=%20animal%20%20'
 
-    returns_tracks ['animal']
+    returns_tracks %w( animal )
   end
 
   def test_that_a_track_that_does_not_exist_returns_empty
     getting '/v2/exercises?tracks=xxx-does-not-exist-xxx'
-    
+
     returns_tracks []
   end
 
@@ -63,8 +63,6 @@ class V1RoutesExerciseFilteringTest < Minitest::Test
   end
 
   def last_tracks
-    json = JSON.parse(last_response.body)
-
-    tracks_received = json['problems'].map{|it| it['track_id']}.uniq
+    JSON.parse(last_response.body)['problems'].map { |it| it['track_id'] }.uniq
   end
 end
--- v1/routes/exercises.rb
+++ v1/routes/exercises.rb
@@ -9,7 +9,9 @@ module V1
 
       get '/v2/exercises' do
         require_key
-        problems = forward_errors { filter(homework.problems, (params[:tracks] || [])) }
+        problems = forward_errors do
+          filter(homework.problems, (params[:tracks] || []))
+        end
         pg :problems, locals: { problems: problems }
       end
 
@@ -41,7 +43,7 @@ module V1
 
       def filter(what, track_ids=[])
         return what if track_ids.empty?
-        what.select{|it| track_ids.include?(it.track_id)}
+        what.select { |it| track_ids.include?(it.track_id) }
       end
     end
   end
