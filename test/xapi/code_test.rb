d4371f93ea2c771d6ce93ea713d037f5c540aa49
Katrina Owen <katrina.owen@gmail.com>
2016-01-20 17:27:26 -0700

    Refactor tests for problem files

--- test/xapi/code_test.rb
+++ test/xapi/code_test.rb
@@ -4,29 +4,20 @@ require 'xapi/code'
 class CodeTest < Minitest::Test
   attr_reader :code
 
-  def path_to(file)
-    "./test/fixtures/tracks/fake/one/#{file}"
-  end
-
-  def setup
-    @code = Xapi::Code.new('./test/fixtures/tracks/fake/one')
-  end
-
-  def test_paths
-    expected = [
-      path_to(".dot"),
-      path_to("Fakefile"),
-      path_to("one_test.tst"),
+  def test_code
+    code = Xapi::Code.new('./test/fixtures/tracks/fake/one')
+    paths = [
+      "./test/fixtures/tracks/fake/one/.dot",
+      "./test/fixtures/tracks/fake/one/Fakefile",
+      "./test/fixtures/tracks/fake/one/one_test.tst",
     ]
-    assert_equal expected, code.paths
-  end
+    assert_equal paths, code.paths
 
-  def test_deliver_applicable_files
-    expected = {
+    files = {
       ".dot" => "dot\n",
       "Fakefile" => "Autorun fake code\n",
       "one_test.tst" => "assert 'one'\n",
     }
-    assert_equal expected, code.files
+    assert_equal files, code.files
   end
 end
