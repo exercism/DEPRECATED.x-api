1345cfdcf3e3bc3bdb3fb1d80d1fb826bbae162c
Katrina Owen <katrina.owen@gmail.com>
2016-05-01 16:19:08 -0600

    Add endpoint for problems without shared data

--- api/v3/routes/problems.rb
+++ api/v3/routes/problems.rb
@@ -1,6 +1,14 @@
 module V3
   module Routes
     class Problems < Core
+      get '/problems/data-todos' do
+        problems = ::Xapi.problems.select { |problem|
+          problem.json_url.nil?
+        }
+
+        { problems: problems.map(&:slug) }.to_json
+      end
+
       get '/problems/:slug/todo' do |slug|
         implementations = ::Xapi.implementations[slug]
         todos = ::Xapi.tracks.reject { |track|
--- test/api/v3/routes/problems_test.rb
+++ test/api/v3/routes/problems_test.rb
@@ -13,4 +13,10 @@ class V3RoutesProblemsTest < Minitest::Test
     body = JSON.parse(last_response.body)
     Approvals.verify(body["problem"], name: 'v3_problem_todo')
   end
+
+  def test_jsons
+    get '/problems/data-todos'
+    body = JSON.parse(last_response.body)
+    Approvals.verify(body["problems"], name: 'v3_problems_shared_data')
+  end
 end
