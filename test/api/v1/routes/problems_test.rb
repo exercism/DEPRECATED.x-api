6ff3376033f49abfaf0b8fc3ebc790e4a50ae702
Geoff Hubbard <ghub@instihost.com>
2016-08-06 16:28:27 +0100

    100% test coverage for v1/problems.

--- test/api/v1/routes/problems_test.rb
+++ test/api/v1/routes/problems_test.rb
@@ -11,12 +11,23 @@ class V1RoutesProblemsTest < Minitest::Test
   def test_full_problems_list
     get '/problems'
     options = { format: :json, name: 'get_full_problems_list' }
+    assert_equal 200, last_response.status
     Approvals.verify(last_response.body, options)
   end
 
   def test_single_problem
     get '/problems/one'
-    options = { format: :json, name: 'get_all_leaps' }
+    options = { format: :json, name: 'get_problems_one' }
+    assert_equal 200, last_response.status
     Approvals.verify(last_response.body, options)
   end
+
+
+  def test_nonexistant_problem
+    get '/problems/nonexistant'
+    options = { format: :json, name: 'get_problems_nonexistant' }
+    assert_equal 404, last_response.status
+    Approvals.verify(last_response.body, options)
+  end
+
 end
