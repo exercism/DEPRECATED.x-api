d49932935212d7d15fc5094142854464b8ea98fa
Katrina Owen <kytrinyx@github.com>
2016-10-16 13:09:51 +0200

    Delete now-obsolete domain logic and submodules
    This is using the trackler gem.

--- lib/xapi.rb
+++ lib/xapi.rb
@@ -19,24 +19,4 @@ module Xapi
   REPO_URL = "https://github.com/exercism/x-api"
   EXERCISM_URL = "https://github.com/exercism/exercism.io"
   CONTRIBUTING_URL = "%s/blob/master/CONTRIBUTING.md" % REPO_URL
-
-  def self.problems
-    @problems ||= Problems.new(ROOT)
-  end
-
-  def self.tracks
-    @tracks ||= Tracks.new(ROOT)
-  end
-
-  # rubocop:disable NegatedIf
-  def self.implementations
-    return @implementations if !!@implementations
-    @implementations = Hash.new { |h, k| h[k] = [] }
-    tracks.each do |track|
-      track.implementations.each do |implementation|
-        @implementations[implementation.problem.slug] << implementation
-      end
-    end
-    @implementations
-  end
 end
