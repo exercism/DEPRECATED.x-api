2d86361919a5d226378f85d74f21042aa77aca04
Katrina Owen <kytrinyx@github.com>
2017-02-03 14:16:31 -0700

    Update cert challenge

--- api/v1/routes/home.rb
+++ api/v1/routes/home.rb
@@ -9,8 +9,8 @@ module V1
         }.to_json
       end
 
-      get '/.well-known/acme-challenge/JHwGfL11srcwjgp4fJvHIwQ8EZzzJ6LRNUG8i2AvT00' do
-        "JHwGfL11srcwjgp4fJvHIwQ8EZzzJ6LRNUG8i2AvT00.Q0_BNqHpf4NmKK5vvJwbO4KBimS1tdy9FH6TtJd91io"
+      get '/.well-known/acme-challenge/Qzooxyz6ib8Ohy8IccdhlKK0QK29SrD5_hQKf-Ay1rg' do
+        "Qzooxyz6ib8Ohy8IccdhlKK0QK29SrD5_hQKf-Ay1rg.Q0_BNqHpf4NmKK5vvJwbO4KBimS1tdy9FH6TtJd91io"
       end
     end
   end
