d0c2064e551d3b62e763bf39cfd225cf7fe69620
Katrina Owen <kytrinyx@github.com>
2017-02-03 14:10:39 -0700

    Add cert challenge

--- api/v1/routes/home.rb
+++ api/v1/routes/home.rb
@@ -8,6 +8,10 @@ module V1
           "build_id" => BUILD_ID,
         }.to_json
       end
+
+      get '/.well-known/acme-challenge/JHwGfL11srcwjgp4fJvHIwQ8EZzzJ6LRNUG8i2AvT00' do
+        "JHwGfL11srcwjgp4fJvHIwQ8EZzzJ6LRNUG8i2AvT00.Q0_BNqHpf4NmKK5vvJwbO4KBimS1tdy9FH6TtJd91io"
+      end
     end
   end
 end
