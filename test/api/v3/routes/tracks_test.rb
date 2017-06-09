9ba807dc50f4fda421843dd9bc4964a4fbc04c89
Katrina Owen <kytrinyx@github.com>
2017-06-09 10:45:34 -0600

    Update trackler

--- test/api/v3/routes/tracks_test.rb
+++ test/api/v3/routes/tracks_test.rb
@@ -12,7 +12,7 @@ class V3RoutesTracksTest < Minitest::Test
     get '/tracks'
 
     tracks = JSON.parse(last_response.body)["tracks"]
-    expected = %w(animal fake fruit jewels shoes snowflake vehicles)
+    expected = %w(animal coins fake fruit jewels shoes snowflake vehicles)
     assert_equal expected, tracks.map { |track| track["id"] }.sort
     Approvals.verify(tracks, name: 'v3_tracks')
 
